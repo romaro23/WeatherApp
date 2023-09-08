deddc9d8787fef6ef83c22f775ec84dd39be85e7
Nahorniy Roman <nahornyi.r22@fpm.dnu.edu.ua>
2023-09-08 10:28:17 +0300

    #Json

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace WeatherApp
 {
@@ -22,7 +26,7 @@ namespace WeatherApp
     
     public partial class MainWindow : Window
     {
-        
+        const string url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/322722?apikey=UDWRwa1QGHyvfKFGDTUZzA1xImcT3FrC";
         public MainWindow()
         {
             InitializeComponent();
@@ -34,9 +38,18 @@ namespace WeatherApp
                 string dayName = currentDate.ToString("dddd");
                 string capitalizedDayName = char.ToUpper(dayName[0]) + dayName.Substring(1);
                 days[i].Content = $"{capitalizedDayName}, {currentDate.Day}";
-
+                getWeather();
             }
+            
 
+        }
+        private async void getWeather()
+        {
+            HttpClient httpClient = new HttpClient();
+            string response = await httpClient.GetStringAsync(url);
+            JObject jsonObject = JObject.Parse(response);
+            double temperatureMiminum = (double)jsonObject["DailyForecasts"][0]["Temperature"]["Minimum"]["Value"];
+            Day1.Content = (temperatureMiminum - 32) * 5/9;
         }
         static DateTime date = DateTime.Now;
         
